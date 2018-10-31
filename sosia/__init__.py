51d13bd1879e81d2af7a09708c2e0d8e4ca9a752
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2018-10-31 11:51:13 +0100

    Make sosia installable

--- sosia/__init__.py
+++ sosia/__init__.py
@@ -1,2 +1,8 @@
+from pbr.version import VersionInfo
+
+_v = VersionInfo('sosia').semantic_version()
+__version__ = _v.release_string()
+version_info = _v.version_tuple()
+
 from sosia.sosia import Original
 from sosia.utils import *
