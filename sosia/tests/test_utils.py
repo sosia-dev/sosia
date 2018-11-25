Outsource margin_range computation and fix bug

sosia/tests/test_utils.py
@@ -10,7 +10,7 @@ from numpy import array
 from scipy.sparse import csr_matrix
 
 from sosia.utils import (FIELDS_SOURCES_LIST, compute_cosine,
-    create_fields_sources_list, raise_non_empty)
+    create_fields_sources_list, margin_range, raise_non_empty)
 
 
 def test_compute_cosine():
@@ -29,6 +29,11 @@ def test_create_fields_sources_list():
     assert_true(isinstance(df, pd.DataFrame))
     assert_equal(list(df.columns), ['asjc', 'source_id', 'type'])
     assert_true(df.shape[0] > 55130)
+
+
+def test_margin_range():
+    assert_equal(margin_range(5, 1), range(4, 7))
+    assert_equal(margin_range(10, 0.09), range(9, 12))
 
 
 def test_raise_non_empty():