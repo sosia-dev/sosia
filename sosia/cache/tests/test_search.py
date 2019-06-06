ceac3d7f363d1f966efcd120854229068f787469
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2019-06-06 23:32:43 +0200

    Apply PEP8, refactor and correct (replace merged author profile)

--- sosia/cache/tests/test_search.py
+++ sosia/cache/tests/test_search.py
@@ -16,139 +16,127 @@ from sosia.cache import (authors_in_cache, author_year_in_cache,
                          cache_sources)
 from sosia.processing import query_year
 from sosia.utils import build_dict, create_cache
-   
+
+test_cache = expanduser("~/.sosia/") + "cache_sqlite_test.sqlite"
+
 
 def test_authors_in_cache():
-    test_file = expanduser("~/.sosia/") + "cache_sqlite_test.sqlite"
-    create_cache(drop=True, file=test_file) 
-    # test empty cache
-    authors = [53164702100,54411022900]
-    authors_df = pd.DataFrame(authors, columns=["auth_id"], dtype="int64")
-    incache, tosearch = authors_in_cache(authors_df, file=test_file)
-    expected_columns = ['auth_id', 'eid', 'surname', 'initials', 'givenname',
-                        'affiliation', 'documents', 'affiliation_id', 'city',
-                        'country', 'areas']
-    assert_equal(tosearch, authors)
+    create_cache(drop=True, file=test_cache)
+    # Variables
+    expected_auth = [53164702100, 57197093438]
+    search_auth = [55317901900]
+    # Test empty cache
+    df1 = pd.DataFrame(expected_auth, columns=["auth_id"])
+    incache, tosearch = authors_in_cache(df1, file=test_cache)
+    expected_cols = ['auth_id', 'eid', 'surname', 'initials', 'givenname',
+                     'affiliation', 'documents', 'affiliation_id', 'city',
+                     'country', 'areas']
+    assert_equal(tosearch, expected_auth)
     assert_equal(len(incache), 0)
-    assert_equal(incache.columns.tolist(), expected_columns)
-    # test partial retrieval
-    q = 'AU-ID(' + ') OR AU-ID('.join([str(a) for a in authors]) + ')'
-    res = AuthorSearch(q).authors
-    res = pd.DataFrame(res)
-    res["auth_id"] = res.apply(lambda x: x.eid.split("-")[-1], axis=1)
-    res = res[["auth_id", "eid", "surname", "initials",
-               "givenname", "affiliation", "documents",
-               "affiliation_id", "city", "country","areas"]]
-    cache_authors(res, file=test_file)
-    authors = [53164702100,54411022900,55317901900]
-    authors_df = pd.DataFrame(authors, columns=["auth_id"], dtype="int64")
-    incache, tosearch = authors_in_cache(authors_df, file=test_file)
+    assert_equal(incache.columns.tolist(), expected_cols)
+    # Test partial retrieval
+    q = "AU-ID({})".format(') OR AU-ID('.join([str(a) for a in expected_auth]))
+    res = pd.DataFrame(AuthorSearch(q).authors)
+    res["auth_id"] = res["eid"].str.split("-").str[-1]
+    res = res[expected_cols]
+    cache_authors(res, file=test_cache)
+    df2 = pd.DataFrame(expected_auth + search_auth, columns=["auth_id"])
+    incache, tosearch = authors_in_cache(df2, file=test_cache)
     assert_equal(tosearch, [55317901900])
     assert_equal(len(incache), 2)
-    # test full retrieval
-    authors = [53164702100,54411022900]
-    authors_df = pd.DataFrame(authors, columns=["auth_id"], dtype="int64")
-    incache, tosearch = authors_in_cache(authors_df, file=test_file)
+    # Test full retrieval
+    incache, tosearch = authors_in_cache(df1, file=test_cache)
     assert_equal(tosearch, [])
     assert_equal(len(incache), 2)
 
 
 def test_author_year_in_cache():
-    test_file = expanduser("~/.sosia/") + "cache_sqlite_test.sqlite"
-    create_cache(drop=True, file=test_file) 
-    # test empty cache
-    authors = [53164702100,54411022900]
-    authors_df = pd.DataFrame(authors, columns=["auth_id"], dtype="int64")
-    authors_df["year"] = 2016
-    author_year_incache, author_year_search = author_year_in_cache(authors_df,
-                                                                file=test_file)
-    assert_frame_equal(author_year_search, authors_df)
-    assert_equal(len(author_year_incache), 0)
-    # test partial retrieval
-    q = ('AU-ID(' + ') OR AU-ID('.join([str(a) for a in authors]) + ')' +
-         " AND PUBYEAR BEF 2017")
-    res = ScopusSearch(q).results
-    res = build_dict(res, authors)
-    res = pd.DataFrame.from_dict(res, orient="index", dtype="int64")
-    res["year"] = 2016
-    res = res[["year", "first_year", "n_pubs", "n_coauth"]]
-    res.reset_index(inplace=True)
-    res.columns = ["auth_id", "year", "first_year", "n_pubs", "n_coauth"]
-    cache_author_year(res, file=test_file)
-    authors = [53164702100,54411022900,55317901900]
-    authors_df = pd.DataFrame(authors, columns=["auth_id"], dtype="int64")
-    authors_df["year"] = 2016
-    author_year_incache, author_year_search = author_year_in_cache(authors_df,
-                                                                file=test_file)
-    assert_equal(author_year_incache.auth_id.tolist(), [53164702100,54411022900])
-    assert_equal(author_year_incache.year.tolist(), [2016,2016])
-    assert_equal(author_year_search.auth_id.tolist(), [55317901900])
-    assert_equal(author_year_search.year.tolist(), [2016])
-    # test full retrieval
-    authors = [53164702100,54411022900]
-    authors_df = pd.DataFrame(authors, columns=["auth_id"], dtype="int64")
-    authors_df["year"] = 2016
-    author_year_incache, author_year_search = author_year_in_cache(authors_df,
-                                                                file=test_file)
-    assert_equal(author_year_incache.auth_id.tolist(), [53164702100,54411022900])
-    assert_equal(author_year_incache.year.tolist(), [2016,2016])
+    create_cache(drop=True, file=test_cache)
+    # Variables
+    expected_auth = [53164702100, 57197093438]
+    search_auth = [55317901900]
+    year = 2016
+    # Test empty cache
+    df1 = pd.DataFrame(expected_auth, columns=["auth_id"])
+    df1["year"] = year
+    auth_y_incache, auth_y_search = author_year_in_cache(df1, file=test_cache)
+    assert_frame_equal(auth_y_search, df1)
+    assert_equal(len(auth_y_incache), 0)
+    # Test partial retrieval
+    fill = ') OR AU-ID('.join([str(a) for a in expected_auth])
+    q = "AU-ID({}) AND PUBYEAR BEF {}".format(fill, year+1)
+    res = build_dict(ScopusSearch(q).results, expected_auth)
+    res = pd.DataFrame.from_dict(res, orient="index")
+    res["year"] = year
+    cols = ["year", "first_year", "n_pubs", "n_coauth"]
+    res = res[cols].reset_index().rename(columns={"index": "auth_id"})
+    cache_author_year(res, file=test_cache)
+    df2 = pd.DataFrame(expected_auth + search_auth,
+                       columns=["auth_id"])
+    df2["year"] = year
+    auth_y_incache, auth_y_search = author_year_in_cache(df2, file=test_cache)
+    assert_equal(auth_y_incache.auth_id.tolist(), expected_auth)
+    assert_equal(auth_y_incache.year.tolist(), [year, year])
+    assert_equal(auth_y_search.auth_id.tolist(), search_auth)
+    assert_equal(auth_y_search.year.tolist(), [year])
+    # Test full retrieval
+    author_year_incache, author_year_search = author_year_in_cache(df1,
+        file=test_cache)
+    assert_equal(author_year_incache.auth_id.tolist(), expected_auth)
+    assert_equal(author_year_incache.year.tolist(), [year, year])
     assert_true(author_year_search.empty)
-    
+
 
 def test_author_size_in_cache():
-    test_file = expanduser("~/.sosia/") + "cache_sqlite_test.sqlite"
-    create_cache(drop=True, file=test_file) 
-    # test empty cache
-    group = [53164702100]
-    years_check = [2010, 2017]
-    authors_df = pd.DataFrame(list(product(group, years_check)),
-                           columns=["auth_id", "year"])
-    types = {"auth_id": int, "year": int}
-    authors_df.astype(types, inplace=True)
-    authors_size = author_size_in_cache(authors_df, file=test_file)
-    assert_equal(len(authors_size), 0)
-    assert_true(isinstance(authors_size,pd.DataFrame))
-    # test add to cache...
-    tp1 = (53164702100, 2010, 0)
-    cache_author_size(tp1, file=test_file)
-    tp2 = (53164702100, 2017, 6)
-    cache_author_size(tp2, file=test_file)
-    # ...and retrieve
-    authors_size = author_size_in_cache(authors_df, file=test_file)
-    assert_equal(len(authors_size), 2)
-    assert_frame_equal(authors_size[["auth_id","year"]], authors_df)
-    assert_equal(authors_size[authors_size.year==2010]["n_pubs"][0], 0)
-    assert_equal(authors_size[authors_size.year==2017]["n_pubs"][1], 6)
+    create_cache(drop=True, file=test_cache)
+    # Variables
+    expected_auth = 53164702100
+    expected_years = [2010, 2017]
+    pubs1 = 0
+    pubs2 = 6
+    cols = ["auth_id", "year"]
+    df = pd.DataFrame(list(product([expected_auth], expected_years)),
+                      columns=cols)
+    # Test empty cache
+    size = author_size_in_cache(df, file=test_cache)
+    assert_equal(len(size), 0)
+    assert_true(isinstance(size, pd.DataFrame))
+    # Test adding to and retrieving from cache
+    tp1 = (expected_auth, expected_years[0], pubs1)
+    cache_author_size(tp1, file=test_cache)
+    tp2 = (expected_auth, expected_years[1], pubs2)
+    cache_author_size(tp2, file=test_cache)
+    size = author_size_in_cache(df, file=test_cache)
+    assert_equal(len(size), 2)
+    assert_frame_equal(size[cols], df)
+    assert_equal(size[size.year == expected_years[0]]["n_pubs"][0], pubs1)
+    assert_equal(size[size.year == expected_years[1]]["n_pubs"][1], pubs2)
 
-    
+
 def test_sources_in_cache():
-    test_file = expanduser("~/.sosia/") + "cache_sqlite_test.sqlite"
-    create_cache(drop=True, file=test_file) 
-    # test empty cache
-    search_sources = [22900]
-    _years_search = [2010,2005]
-    sources_ys = pd.DataFrame(list(product(search_sources, _years_search)),
-                              columns=["source_id", "year"])
-    types = {"source_id": int, "year": int}
-    sources_ys.astype(types, inplace=True)
-    sources_ys_incache, sources_ys_search = sources_in_cache(sources_ys,
-                                                             file=test_file)
-    assert_frame_equal(sources_ys_search, sources_ys)
+    create_cache(drop=True, file=test_cache)
+    # Variables
+    expected_sources = [22900]
+    expected_years = [2010, 2005]
+    cols = ["source_id", "year"]
+    # Test empty cache
+    df = pd.DataFrame(list(product(expected_sources, expected_years)),
+                      columns=cols)
+    sources_ys_incache, sources_ys_search = sources_in_cache(df, file=test_cache)
+    assert_frame_equal(sources_ys_search, df)
     assert_true(sources_ys_incache.empty)
-    # test partial retrieval
-    res = query_year(2010,search_sources,False,False)
-    cache_sources(res, file=test_file)
+    # Test partial retrieval
+    res = query_year(expected_years[0], expected_sources, False, False)
+    cache_sources(res, file=test_cache)
+    sources_ys_incache, sources_ys_search = sources_in_cache(df, file=test_cache)
+    assert_equal(sources_ys_incache.source_id.tolist(), expected_sources)
+    assert_equal(sources_ys_incache.year.tolist(), [expected_years[0]])
+    assert_equal(sources_ys_search.source_id.tolist(), expected_sources)
+    assert_equal(sources_ys_search.year.tolist(), [expected_years[1]])
+    # Test full retrieval
+    sources_ys = sources_ys_incache[cols]
     sources_ys_incache, sources_ys_search = sources_in_cache(sources_ys,
-                                                             file=test_file)
-    assert_equal(sources_ys_incache.source_id.tolist(), [22900])
-    assert_equal(sources_ys_incache.year.tolist(), [2010])
-    assert_equal(sources_ys_search.source_id.tolist(), [22900])
-    assert_equal(sources_ys_search.year.tolist(), [2005])
-    # test full retrieval
-    sources_ys = sources_ys_incache[["source_id","year"]]
-    sources_ys_incache, sources_ys_search = sources_in_cache(sources_ys,
-                                                             file=test_file)
-    assert_equal(sources_ys_incache.source_id.tolist(), [22900])
-    assert_equal(sources_ys_incache.year.tolist(), [2010])
+                                                             file=test_cache)
+    assert_equal(sources_ys_incache.source_id.tolist(), expected_sources)
+    assert_equal(sources_ys_incache.year.tolist(), [expected_years[0]])
     assert_true(sources_ys_search.empty)
-    
