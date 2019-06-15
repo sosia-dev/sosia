6d37adef2d88c74858ac6c2e3ed6341e7d7e457e
Stefano Baruffaldi <ste.baruffaldi@gmail.com>
2019-06-15 14:44:34 +0100

    Update test_search
    The tests or the module can be improved to be more robust to changes in types

--- sosia/cache/tests/test_search.py
+++ sosia/cache/tests/test_search.py
@@ -24,21 +24,23 @@ def test_authors_in_cache():
     expected_auth = ["53164702100", "57197093438"]
     search_auth = ["55317901900"]
     # Test empty cache
-    df1 = pd.DataFrame(expected_auth, columns=["auth_id"])
+    df1 = pd.DataFrame(expected_auth, columns=["auth_id"], dtype="int64")
     incache, tosearch = authors_in_cache(df1, file=test_cache)
     expected_cols = ['auth_id', 'eid', 'surname', 'initials', 'givenname',
                      'affiliation', 'documents', 'affiliation_id', 'city',
                      'country', 'areas']
+    expected_auth = [int(au) for au in expected_auth]
     assert_equal(tosearch, expected_auth)
     assert_equal(len(incache), 0)
     assert_equal(incache.columns.tolist(), expected_cols)
     # Test partial retrieval
     q = "AU-ID({})".format(') OR AU-ID('.join([str(a) for a in expected_auth]))
-    res = pd.DataFrame(AuthorSearch(q).authors)
+    res = pd.DataFrame(AuthorSearch(q).authors, dtype="int64")
     res["auth_id"] = res["eid"].str.split("-").str[-1]
     res = res[expected_cols]
     cache_insert(res, table="authors", file=test_cache)
-    df2 = pd.DataFrame(expected_auth + search_auth, columns=["auth_id"])
+    df2 = pd.DataFrame(expected_auth + search_auth, columns=["auth_id"],
+                       dtype="int64")
     incache, tosearch = authors_in_cache(df2, file=test_cache)
     assert_equal(tosearch, [55317901900])
     assert_equal(len(incache), 2)
@@ -55,24 +57,27 @@ def test_author_year_in_cache():
     search_auth = ["55317901900"]
     year = 2016
     # Test empty cache
-    df1 = pd.DataFrame(expected_auth, columns=["auth_id"])
+    df1 = pd.DataFrame(expected_auth, columns=["auth_id"],
+                       dtype="int64")
     df1["year"] = year
     auth_y_incache, auth_y_search = author_year_in_cache(df1, file=test_cache)
     assert_frame_equal(auth_y_search, df1)
     assert_equal(len(auth_y_incache), 0)
     # Test partial retrieval
     fill = ') OR AU-ID('.join([str(a) for a in expected_auth])
-    q = "AU-ID({}) AND PUBYEAR BEF {}".format(fill, year+1)
+    q = "(AU-ID({})) AND PUBYEAR BEF {}".format(fill, year+1)
     res = build_dict(ScopusSearch(q).results, expected_auth)
-    res = pd.DataFrame.from_dict(res, orient="index")
+    res = pd.DataFrame.from_dict(res, orient="index", dtype="int64")
     res["year"] = year
     cols = ["year", "first_year", "n_pubs", "n_coauth"]
     res = res[cols].reset_index().rename(columns={"index": "auth_id"})
     cache_insert(res, table="author_year", file=test_cache)
     df2 = pd.DataFrame(expected_auth + search_auth,
-                       columns=["auth_id"])
+                       columns=["auth_id"], dtype="int64")
     df2["year"] = year
     auth_y_incache, auth_y_search = author_year_in_cache(df2, file=test_cache)
+    expected_auth = [int(au) for au in expected_auth]
+    search_auth = [int(au) for au in search_auth]
     assert_equal(auth_y_incache.auth_id.tolist(), expected_auth)
     assert_equal(auth_y_incache.year.tolist(), [year, year])
     assert_equal(auth_y_search.auth_id.tolist(), search_auth)
@@ -89,12 +94,12 @@ def test_author_size_in_cache():
     create_cache(drop=True, file=test_cache)
     # Variables
     expected_auth = "53164702100"
-    expected_years = ["2010", "2017"]
+    expected_years = [2010, 2017]
     pubs1 = 0
     pubs2 = 6
     cols = ["auth_id", "year"]
     df = pd.DataFrame(list(product([expected_auth], expected_years)),
-                      columns=cols)
+                      columns=cols, dtype="int64")
     # Test empty cache
     size = author_size_in_cache(df, file=test_cache)
     assert_equal(len(size), 0)
@@ -115,11 +120,11 @@ def test_sources_in_cache():
     create_cache(drop=True, file=test_cache)
     # Variables
     expected_sources = ["22900"]
-    expected_years = ["2010", "2005"]
+    expected_years = [2010, 2005]
     cols = ["source_id", "year"]
     # Test empty cache
     df = pd.DataFrame(list(product(expected_sources, expected_years)),
-                      columns=cols)
+                      columns=cols, dtype="int64")
     sources_ys_incache, sources_ys_search = sources_in_cache(df, file=test_cache)
     assert_frame_equal(sources_ys_search, df)
     assert_true(sources_ys_incache.empty)
@@ -127,6 +132,7 @@ def test_sources_in_cache():
     res = query_year(expected_years[0], expected_sources, False, False)
     cache_insert(res, table="sources", file=test_cache)
     sources_ys_incache, sources_ys_search = sources_in_cache(df, file=test_cache)
+    expected_sources = [int(s) for s in expected_sources]
     assert_equal(sources_ys_incache.source_id.tolist(), expected_sources)
     assert_equal(sources_ys_incache.year.tolist(), [expected_years[0]])
     assert_equal(sources_ys_search.source_id.tolist(), expected_sources)
@@ -135,6 +141,7 @@ def test_sources_in_cache():
     sources_ys = sources_ys_incache[cols]
     sources_ys_incache, sources_ys_search = sources_in_cache(sources_ys,
                                                              file=test_cache)
+    expected_sources = [int(s) for s in expected_sources]
     assert_equal(sources_ys_incache.source_id.tolist(), expected_sources)
     assert_equal(sources_ys_incache.year.tolist(), [expected_years[0]])
     assert_true(sources_ys_search.empty)
