48a05cd2a4e9b185c798ec4c031c747a3f59d3ca
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2020-09-01 14:21:14 +0200

    Streamline test_retrieving module, split tests

--- sosia/processing/caching/tests/test_retrieving.py
+++ sosia/processing/caching/tests/test_retrieving.py
@@ -15,6 +15,7 @@ from sosia.processing import build_dict, insert_data, retrieve_authors,\
     robust_join, query_pubs_by_sourceyear
 
 test_cache = expanduser("~/.sosia/test.sqlite")
+refresh = 30
 
 
 def test_retrieve_authors():
@@ -22,184 +23,102 @@ def test_retrieve_authors():
     conn = connect_database(test_cache)
     # Variables
     expected_auth = [53164702100, 57197093438]
-    search_auth = [55317901900]
-    # Test empty cache
-    df1 = pd.DataFrame(expected_auth, columns=["auth_id"], dtype="int64")
-    incache, tosearch = retrieve_authors(df1, conn)
+    df = pd.DataFrame(expected_auth, columns=["auth_id"], dtype="int64")
     expected_cols = ['auth_id', 'eid', 'surname', 'initials', 'givenname',
                      'affiliation', 'documents', 'affiliation_id', 'city',
                      'country', 'areas']
-    assert_equal(tosearch, expected_auth)
-    assert_equal(len(incache), 0)
-    assert_equal(incache.columns.tolist(), expected_cols)
-    # Test partial retrieval
+    # Retrieve data
+    incache, missing = retrieve_authors(df, conn)
+    assert_equal(incache.shape[0], 0)
+    assert_equal(incache.columns.to_list(), expected_cols)
+    assert_equal(missing, expected_auth)
+
+
+def test_retrieve_authors_insert():
+    make_database(test_cache, drop=True)
+    conn = connect_database(test_cache)
+    # Variables
+    expected_auth = [53164702100, 57197093438]
+    search_auth = [55317901900]
+    expected_cols = ['auth_id', 'eid', 'surname', 'initials', 'givenname',
+                     'affiliation', 'documents', 'affiliation_id', 'city',
+                     'country', 'areas']
+    # Insert data
     q = f"AU-ID({robust_join(expected_auth, sep=') OR AU-ID(')})"
-    res = pd.DataFrame(AuthorSearch(q).authors, dtype="int64")
+    res = pd.DataFrame(AuthorSearch(q, refresh=refresh).authors, dtype="int64")
     res["auth_id"] = res["eid"].str.split("-").str[-1]
     res = res[expected_cols]
     insert_data(res, conn, table="authors")
-    df2 = pd.DataFrame(expected_auth + search_auth, columns=["auth_id"],
-                       dtype="int64")
-    incache, tosearch = retrieve_authors(df2, conn)
-    assert_equal(tosearch, [55317901900])
-    assert_equal(len(incache), 2)
-    # Test full retrieval
-    incache, tosearch = retrieve_authors(df1, conn)
-    assert_equal(tosearch, [])
-    assert_equal(len(incache), 2)
+    # Retrieve data
+    df = pd.DataFrame(expected_auth + search_auth, columns=["auth_id"],
+                      dtype="int64")
+    incache, missing = retrieve_authors(df, conn)
+    assert_equal(incache.shape[0], 2)
+    assert_equal(missing, [55317901900])
 
 
 def test_retrieve_author_pubs():
     make_database(test_cache, drop=True)
     conn = connect_database(test_cache)
+    df = pd.DataFrame(product([53164702100], [2010, 2017]),
+                      columns=["auth_id", "year"], dtype="int64")
+    pubs = retrieve_author_pubs(df, conn)
+    assert_equal(pubs.shape[0], 0)
+    assert_true(isinstance(pubs, pd.DataFrame))
+
+
+def test_retrieve_author_pubs_insert():
+    make_database(test_cache, drop=True)
+    conn = connect_database(test_cache)
     # Variables
-    expected_auth = 53164702100
-    expected_years = [2010, 2017]
-    pubs1 = 0
-    pubs2 = 6
-    cols = ["auth_id", "year"]
-    df = pd.DataFrame(product([expected_auth], expected_years),
-                      columns=cols, dtype="int64")
-    # Test empty cache
-    size = retrieve_author_pubs(df, conn)
-    assert_equal(len(size), 0)
-    assert_true(isinstance(size, pd.DataFrame))
-    # Test adding to and retrieving from cache
-    tp1 = (expected_auth, expected_years[0], pubs1)
-    insert_data(tp1, conn, table="author_pubs")
-    tp2 = (expected_auth, expected_years[1], pubs2)
-    insert_data(tp2, conn, table="author_pubs")
-    size = retrieve_author_pubs(df, conn)
-    assert_equal(len(size), 2)
-    assert_frame_equal(size[cols], df)
-    assert_equal(size[size.year == expected_years[0]]["n_pubs"][0], pubs1)
-    assert_equal(size[size.year == expected_years[1]]["n_pubs"][1], pubs2)
+    data = {"auth_id": [53164702100, 53164702100],
+            "year": [2010, 2017], "n_pubs": [0, 6]}
+    expected = pd.DataFrame(data, dtype="int64")
+    # Insert data
+    insert_data(expected.iloc[0].values, conn, table="author_pubs")
+    insert_data(expected.iloc[1].values, conn, table="author_pubs")
+    # Retrieve data
+    received = retrieve_author_pubs(expected[["auth_id", "year"]], conn)
+    assert_frame_equal(received, expected)
 
 
 def test_retrieve_authors_year():
+    make_database(test_cache, drop=True)
+    conn = connect_database(test_cache)
+    data = {"auth_id": [53164702100, 57197093438], "year": [2016, 2016]}
+    expected = pd.DataFrame(data, dtype="int64")
+    incache, missing = retrieve_authors_year(expected, conn)
+    assert_true(incache.empty)
+    assert_frame_equal(missing, expected)
+
+
+def test_retrieve_authors_year_insert():
     make_database(test_cache, drop=True)
     conn = connect_database(test_cache)
     # Variables
     expected_auth = [53164702100, 57197093438]
     search_auth = [55317901900]
     year = 2016
-    # Test empty cache
-    df1 = pd.DataFrame(expected_auth, columns=["auth_id"], dtype="int64")
-    df1["year"] = year
-    auth_y_incache, auth_y_search = retrieve_authors_year(df1, conn)
-    assert_frame_equal(auth_y_search, df1)
-    assert_equal(len(auth_y_incache), 0)
-    # Test partial retrieval
-    fill = robust_join(expected_auth, sep=') OR AU-ID(')
-    q = f"(AU-ID({fill})) AND PUBYEAR BEF {year+1}"
-    res = build_dict(ScopusSearch(q).results, expected_auth)
-    res = pd.DataFrame.from_dict(res, orient="index", dtype="int64")
-    res["year"] = year
-    cols = ["year", "first_year", "n_pubs", "n_coauth"]
-    res = res[cols].reset_index().rename(columns={"index": "auth_id"})
-    insert_data(res, conn, table="author_year")
     df2 = pd.DataFrame(expected_auth + search_auth,
                        columns=["auth_id"], dtype="int64")
     df2["year"] = year
-    auth_y_incache, auth_y_search = retrieve_authors_year(df2, conn)
-    assert_equal(sorted(auth_y_incache.auth_id.tolist()), expected_auth)
-    assert_equal(auth_y_incache.year.tolist(), [year, year])
-    assert_equal(auth_y_search.auth_id.tolist(), search_auth)
-    assert_equal(auth_y_search.year.tolist(), [year])
-    # Test full retrieval
-    auth_year_incache, auth_year_search = retrieve_authors_year(df1, conn)
-    assert_equal(sorted(auth_year_incache.auth_id.tolist()), expected_auth)
-    assert_equal(auth_year_incache.year.tolist(), [year, year])
-    assert_true(auth_year_search.empty)
+    # Insert data
+    fill = robust_join(expected_auth, sep=') OR AU-ID(')
+    q = f"(AU-ID({fill})) AND PUBYEAR BEF {year+1}"
+    d = build_dict(ScopusSearch(q, refresh=refresh).results, expected_auth)
+    expected = pd.DataFrame.from_dict(d, orient="index", dtype="int64")
+    expected = expected.sort_index().rename_axis('auth_id').reset_index()
+    expected["year"] = year
+    expected = expected[['auth_id', 'year', 'first_year', 'n_pubs', 'n_coauth']]
+    insert_data(expected, conn, table="author_year")
+    # Retrieve data
+    incache, missing = retrieve_authors_year(df2, conn)
+    assert_frame_equal(incache, expected)
+    assert_equal(missing['auth_id'].tolist(), search_auth)
+    assert_equal(missing['year'].tolist(), [year])
 
 
-def test_retrieve_sources_empty():
-    make_database(test_cache, drop=True)
-    conn = connect_database(test_cache)
-    df = pd.DataFrame(product([22900], [2005, 2010]),
-                      columns=["source_id", "year"], dtype="int64")
-    sources_ys_incache, sources_ys_search = retrieve_authors_from_sourceyear(df, conn)
-    assert_frame_equal(sources_ys_search, df)
-    assert_true(sources_ys_incache.empty)
-
-
-def test_retrieve_sources_partial():
-    make_database(test_cache, drop=True)
-    conn = connect_database(test_cache)
-    # Variables
-    expected_sources = [22900, 22900]
-    expected_years = [2005, 2010]
-    df = pd.DataFrame({"source_id": expected_sources, "year": expected_years},
-                      dtype="int64")
-    # Populate cache
-    res = query_pubs_by_sourceyear(expected_sources, expected_years[0])
-    res["auids"] = res["auids"].str.split(";")
-    insert_data(res.copy(), conn, table="sources")
-    # Retrieve from cache
-    sources_ys_incache, sources_ys_search = retrieve_authors_from_sourceyear(df, conn)
-    cols = ["source_id", "year"]
-    assert_frame_equal(sources_ys_incache[cols], df.head(1))
-    assert_frame_equal(sources_ys_search[cols], df.tail(1))
-
-
-def test_retrieve_sources_full():
-    make_database(test_cache, drop=True)
-    conn = connect_database(test_cache)
-    # Variables
-    expected_sources = [22900]
-    expected_years = [2005, 2010]
-    cols = ["source_id", "year"]
-    df = pd.DataFrame(product(expected_sources, expected_years),
-                      columns=cols, dtype="int64")
-    # Populate cache
-    res = query_pubs_by_sourceyear(expected_sources, expected_years[0])
-    insert_data(res, conn, table="sources")
-    sources_ys_incache, sources_ys_search = retrieve_authors_from_sourceyear(df, conn)
-    # Retrieve from cache
-    sources_ys = sources_ys_incache[cols]
-    sources_ys_incache, sources_ys_search = retrieve_authors_from_sourceyear(sources_ys, conn)
-    expected_sources = [int(s) for s in expected_sources]
-    assert_equal(sources_ys_incache.source_id.tolist(), expected_sources)
-    assert_equal(sources_ys_incache.year.tolist(), [expected_years[0]])
-    assert_true(sources_ys_search.empty)
-    
-
-def test_sources_afids_in_cache_empty():
-    make_database(test_cache, drop=True)
-    conn = connect_database(test_cache)
-    df = pd.DataFrame(product([22900], [2005, 2010]),
-                      columns=["source_id", "year"], dtype="int64")
-    sa_incache, sa_search = retrieve_authors_from_sourceyear(df, conn, afid=True)
-    assert_frame_equal(sa_search, df)
-    assert_true(sa_incache.empty)
-
-
-def test_sources_afids_in_cache_partial():
-    make_database(test_cache, drop=True)
-    conn = connect_database(test_cache)
-    # Variables
-    expected_sources = [22900]
-    expected_years = [2005, 2010]
-    df = pd.DataFrame(product(expected_sources, expected_years),
-                      columns=["source_id", "year"], dtype="int64")
-    sa_incache, sa_search = retrieve_authors_from_sourceyear(df, conn, afid=True)
-    # Populate cache
-    res = query_pubs_by_sourceyear(expected_sources, expected_years[0], afid=True)
-    insert_data(res, conn, table="sources_afids")
-    # Retrieve from cache
-    sa_incache, sa_search = retrieve_authors_from_sourceyear(df, conn, afid=True)
-    expected_sources = set([int(s) for s in expected_sources])
-    assert_equal(set(sa_incache.source_id.tolist()), set(expected_sources))
-    assert_equal(set(sa_incache.year.tolist()), set([expected_years[0]]))
-    assert_equal(set(sa_search.source_id.tolist()), set(expected_sources))
-    assert_equal(set(sa_search.year.tolist()), set([expected_years[1]]))
-    expected = range(125-5, 125+5)
-    print(len(sa_incache))
-    assert_true(len(sa_incache) in expected)
-    assert_true(len(sa_incache.afid.drop_duplicates()) in expected)
-
-
-def test_sources_afids_in_cache_full():
+def test_retrieve_authors_from_sourceyear_sources():
     make_database(test_cache, drop=True)
     conn = connect_database(test_cache)
     # Variables
@@ -210,35 +129,30 @@ def test_sources_afids_in_cache_full():
                       columns=cols, dtype="int64")
     # Populate cache
     res = query_pubs_by_sourceyear(expected_sources, expected_years[0], afid=True)
-    insert_data(res, conn, table="sources_afids")
+    insert_data(res, conn, table="sources")
     # Retrieve from cache
-    sa_incache, sa_search = retrieve_authors_from_sourceyear(df, conn, afid=True)
-    sources_ys = sa_incache[cols].drop_duplicates()
-    sa_incache, sa_search = retrieve_authors_from_sourceyear(sources_ys, conn, afid=True)
-    expected_sources = set([int(s) for s in expected_sources])
-    assert_equal(set(sa_incache.source_id.tolist()), set(expected_sources))
-    assert_equal(set(sa_incache.year.tolist()), set([expected_years[0]]))
-    expected = range(125-5, 125+5)
-    assert_true(len(sa_incache) in expected)
-    assert_true(len(sa_incache.afid.drop_duplicates()) in expected)
-    assert_true(sa_search.empty)
+    incache, missing = retrieve_authors_from_sourceyear(df, conn)
+    assert_frame_equal(incache[cols], df.head(1))
+    assert_frame_equal(missing[cols], df.tail(1))
 
 
-def test_sources_afids_in_sources_cache():
+def test_retrieve_authors_from_sourceyear_sources_afids():
     make_database(test_cache, drop=True)
     conn = connect_database(test_cache)
     # Variables
     expected_sources = [22900]
     expected_years = [2005, 2010]
+    expected_range = range(125-5, 125+5)
     df = pd.DataFrame(product(expected_sources, expected_years),
                       columns=["source_id", "year"], dtype="int64")
     # Populate cache
     res = query_pubs_by_sourceyear(expected_sources, expected_years[0], afid=True)
-    insert_data(res, conn, table="sources")
+    insert_data(res, conn, table="sources_afids")
     # Retrieve from cache
-    sources_ys_incache, sources_ys_search = retrieve_authors_from_sourceyear(df, conn)
-    expected_sources = [int(s) for s in expected_sources]
-    assert_equal(sources_ys_incache.source_id.tolist(), expected_sources)
-    assert_equal(sources_ys_incache.year.tolist(), [expected_years[0]])
-    assert_equal(sources_ys_search.source_id.tolist(), expected_sources)
-    assert_equal(sources_ys_search.year.tolist(), [expected_years[1]])
+    incache, missing = retrieve_authors_from_sourceyear(df, conn, afid=True)
+    assert_equal(incache['source_id'].unique(), expected_sources)
+    assert_equal(incache['year'].unique(), [expected_years[0]])
+    assert_true(incache.shape[0] in expected_range)
+    assert_true(incache['afid'].nunique() in expected_range)
+    assert_equal(missing['source_id'].unique(), expected_sources)
+    assert_equal(missing['year'].unique(), [expected_years[1]])
