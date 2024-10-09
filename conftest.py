81a1f39f3e442645da7af8dfeeec951a47d0978f
Michael E. Rose <Michael.Ernst.Rose@gmail.com>
2024-10-09 16:07:55 +0200

    Clean up test scripts

--- conftest.py
+++ conftest.py
@@ -9,6 +9,7 @@ from sosia.establishing import connect_database, make_database
 
 init()
 
+
 @pytest.fixture(scope="session")
 def test_cache():
     test_cache = Path.home() / ".cache" / "sosia" / "test.sqlite"
@@ -16,25 +17,30 @@ def test_cache():
     make_database(test_cache)
     return test_cache
 
+
 @pytest.fixture
 def test_conn(test_cache):
     conn = connect_database(test_cache)
     yield conn
     conn.close()
 
+
 @pytest.fixture(scope="session")
 def refresh_interval():
     return 30
+
 
 @pytest.fixture(scope="session")
 def scientist1(test_cache, refresh_interval):
     return Scientist([6701809842], 2001, refresh=refresh_interval,
                      db_path=test_cache)
 
+
 @pytest.fixture(scope="session")
 def scientist2(test_cache, refresh_interval):
     return Scientist([55208373700, 55208373700], 2017,
                      refresh=refresh_interval, db_path=test_cache)
+
 
 @pytest.fixture(scope="session")
 def scientist3(test_cache, refresh_interval):
@@ -42,16 +48,19 @@ def scientist3(test_cache, refresh_interval):
     return Scientist([55208373700], 2017, eids=eids,
                      refresh=refresh_interval, db_path=test_cache)
 
+
 @pytest.fixture(scope="session")
 def scientist4(test_cache, refresh_interval):
     return Scientist([55208373700], 2015, refresh=refresh_interval,
                      db_path=test_cache)
+
 
 @pytest.fixture(scope="session")
 def original1(test_cache, refresh_interval):
     return Original(55208373700, 2017, cits_margin=200, first_year_margin=1,
                     pub_margin=0.1, coauth_margin=0.1, db_path=test_cache,
                     refresh=refresh_interval)
+
 
 @pytest.fixture(scope="session")
 def original2(test_cache, refresh_interval):
--- sosia/processing/tests/test_getting.py
+++ sosia/processing/tests/test_getting.py
@@ -2,8 +2,8 @@
 
 from pandas import DataFrame
 
-from sosia.processing import get_author_data, get_citations
-from sosia.processing.getting import get_author_info
+from sosia.processing.getting import get_author_data, get_author_info, \
+    get_citations
 
 
 def test_get_author_data(test_conn):
@@ -17,18 +17,7 @@ def test_get_author_data(test_conn):
     assert data["auth_id"].nunique() == len(group)
 
 
-def test_get_citations(test_conn):
-    group = [6701809842, 16319073600, 6602070937]
-    data = get_citations(group, year=2023, conn=test_conn)
-    assert isinstance(data, DataFrame)
-    expected_cols = ['auth_id', 'n_cits']
-    assert list(data.columns) == expected_cols
-    assert data["auth_id"].nunique() == len(group)
-    assert data.shape[0] == len(group)
-    assert data.loc[0, "n_cits"] > 4000
-
-
-def test_query_authors(test_conn, refresh_interval):
+def test_get_author_info(test_conn, refresh_interval):
     auth_list = [6701809842, 55208373700]
     auth_data = get_author_info(auth_list, test_conn, refresh=refresh_interval)
     assert isinstance(auth_data,  DataFrame)
@@ -38,3 +27,14 @@ def test_query_authors(test_conn, refresh_interval):
     assert auth_data.columns.tolist() == expected_cols
     assert auth_data["auth_id"].tolist() == auth_list
     assert auth_data["surname"].tolist() == ["Harhoff", "Baruffaldi"]
+
+
+def test_get_citations(test_conn):
+    group = [6701809842, 16319073600, 6602070937]
+    data = get_citations(group, year=2023, conn=test_conn)
+    assert isinstance(data, DataFrame)
+    expected_cols = ['auth_id', 'n_cits']
+    assert list(data.columns) == expected_cols
+    assert data["auth_id"].nunique() == len(group)
+    assert data.shape[0] == len(group)
+    assert data.loc[0, "n_cits"] > 4000
